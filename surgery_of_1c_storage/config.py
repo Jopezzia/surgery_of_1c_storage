1796db74d3258820a8c53c6875f6de79f943702a
Антон Антонов <antonov@ibs.ru>
2023-01-21 06:36:42 +0300

    Add env vars and dockerfile

--- surgery_of_1c_storage/config.py
+++ surgery_of_1c_storage/config.py
@@ -13,44 +13,68 @@ def init_config(config_file_name):
 
 
 def create_config_file(filename, db_type):
+
     with open(filename, 'w') as f:
         if db_type == 'psql':
-            f.write(template_config_file_psql.strip())
+            f.write(get_template_config_psql().strip())
         elif db_type == 'mssql':
-            f.write(template_config_file_mssql.strip())
+            f.write(get_template_config_mssql.strip())
         else:
             raise Exception('Неизвестный тип СУБД')
     print(f'Шаблон конфигурационного файла для {db_type} сохранен в {filename}')
 
+def get_template_config_psql():
 
-template_config_file_psql = """
-[Database]
-TYPE = Postgres
-SERVER = host
-USER = postgres
-PASSWORD = postgres
-BASENAME = infobase
-PORT = 5432
+    db_type = os.getenv('SOOC_DB_TYPE') if os.getenv('SOOC_DB_TYPE') != None else "Postgres"
+    server = os.getenv('SOOC_DB_SERVER') if os.getenv('SOOC_DB_SERVER') != None else "host"
+    user = os.getenv('SOOC_DB_USER') if os.getenv('SOOC_DB_USER') != None else "postgres"
+    password = os.getenv('SOOC_DB_PASSWORD') if os.getenv('SOOC_DB_PASSWORD') != None else "postgres"
+    basename = os.getenv('SOOC_DB_BASENAME') if os.getenv('SOOC_DB_BASENAME') != None else "infobase"
+    port = os.getenv('SOOC_DB_PORT') if os.getenv('SOOC_DB_PORT') != None else "5432"
+    tables_with_config_file = os.getenv('SOOC_TABLES_WITH_CONFIG_FILE') if os.getenv('SOOC_TABLES_WITH_CONFIG_FILE') != None else "params,files,config,configsave,configcas,configcassave"
+    file_encode_mode = os.getenv('SOOC_FILE_ENCODE_MODE') if os.getenv('SOOC_FILE_ENCODE_MODE') != None else ".encode_modes"
+    template_config_file_psql = f"""
+    [Database]
+    TYPE = {db_type}
+    SERVER = {server}
+    USER = {user}
+    PASSWORD = {password}
+    BASENAME = {basename}
+    PORT = {port}
 
-[InnerStructureKeys]
-TABLES_WITH_CONFIG_FILE = params,files,config,configsave,configcas,configcassave
+    [InnerStructureKeys]
+    TABLES_WITH_CONFIG_FILE = {tables_with_config_file}
 
-[Coder]
-FILE_ENCODE_MODE = .encode_modes
-"""
+    [Coder]
+    FILE_ENCODE_MODE = {file_encode_mode}
+    """
+    return template_config_file_psql
 
-template_config_file_mssql = """
-[Database]
-TYPE = MSSQL
-SERVER = host
-USER = USR1CV8
-PASSWORD = USR1CV8
-BASENAME = infobase
-ODBC_DRIVER={SQL Server Native Client 11.0}
+def get_template_config_mssql():
+    
+    db_type = os.getenv('SOOC_DB_TYPE') if os.getenv('SOOC_DB_TYPE') != None else "Postgres"
+    server = os.getenv('SOOC_DB_SERVER') if os.getenv('SOOC_DB_SERVER') != None else "host"
+    user = os.getenv('SOOC_DB_USER') if os.getenv('SOOC_DB_USER') != None else "postgres"
+    password = os.getenv('SOOC_DB_PASSWORD') if os.getenv('SOOC_DB_PASSWORD') != None else "postgres"
+    basename = os.getenv('SOOC_DB_BASENAME') if os.getenv('SOOC_DB_BASENAME') != None else "infobase"
+    port = os.getenv('SOOC_DB_PORT') if os.getenv('SOOC_DB_PORT') != None else "5432"
+    tables_with_config_file = os.getenv('SOOC_TABLES_WITH_CONFIG_FILE') if os.getenv('SOOC_TABLES_WITH_CONFIG_FILE') != None else "params,files,config,configsave,configcas,configcassave"
+    file_encode_mode = os.getenv('SOOC_FILE_ENCODE_MODE') if os.getenv('SOOC_FILE_ENCODE_MODE') != None else ".encode_modes"
+    odbc_driver = "{SQL Server Native Client 11.0}"
+    template_config_file_mssql = f"""
+    [Database]
+    TYPE = {db_type}
+    SERVER = {server}
+    USER = {user}
+    PASSWORD = {password}
+    BASENAME = {basename}
+    PORT = {port}
+    ODBC_DRIVER={odbc_driver}
 
-[InnerStructureKeys]
-TABLES_WITH_CONFIG_FILE = params,files,config,configsave,configcas,configcassave
+    [InnerStructureKeys]
+    TABLES_WITH_CONFIG_FILE = {tables_with_config_file}
 
-[Coder]
-FILE_ENCODE_MODE = .encode_modes
-"""
+    [Coder]
+    FILE_ENCODE_MODE = {file_encode_mode}
+    """
+    return template_config_file_mssql
